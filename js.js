fe6f7299d32888519196b6a591232684cc48451f
kokos Rybnik <vojtabern@gmail.com>
2020-11-17 18:55:18 +0100

    third commit

--- js.js
+++ js.js
@@ -4,32 +4,31 @@
 document.getElementById("tlac").addEventListener("click", function() {
     
     let a = document.getElementById("a").value;
-    let o = 4*a;
-    let s = a*a;
+    let o = Math.abs(4*a);
+    let s = Math.abs(a*a);
     console.log(a);
-    document.getElementById("lead").innerHTML = "Obvod je " + o + " cm" + '<br/>' + "Obsah je " + s + " cm" ;
+    document.getElementById("lead").innerHTML = "Obvod je " + o + " cm" + '<br>' + "Obsah je " + s + " cm<sup>2</sup>" ;
 });
 
 document.getElementById("tlacit").addEventListener("click", function() {
     
-    let a = document.getElementById("a1").value;
-    let b = document.getElementById("b1").value;
-    let c = document.getElementById("c1").value;
-    let va = document.getElementById("va1").value;
-    let o = a+b+c;
-    let s = a*va/2;
-    console.log(b);
-    document.getElementById("lead1").innerHTML = "Obvod je " + o + " cm" + '<br/>' + "Obsah je " + s + " cm" ;
+    let a = parseInt(document.getElementById("a1").value);
+    let b = parseInt(document.getElementById("b1").value);
+    let c = parseInt(document.getElementById("c1").value);
+    let va = parseInt(document.getElementById("va1").value);
+    let o= Math.abs(a+b+c);
+    let s = Math.abs(a*va/2);
+    document.getElementById("lead1").innerHTML = "Obvod je " + o + " cm" + '<br>' + "Obsah je " + s + " cm<sup>2</sup>" ;
 });
 
 document.getElementById("tlacitko").addEventListener("click", function() {
     
-    let a = document.getElementById("a2").value;
-    let b = document.getElementById("b2").value;
-    let o = a+a+b+b;
-    let s = a*b;
+    let a = parseInt(document.getElementById("a2").value);
+    let b = parseInt(document.getElementById("b2").value);
+    let o = (2*(Math.abs(a)+ Math.abs(b)));
+    let s = Math.abs(a*b);
     console.log(b);
-    document.getElementById("lead2").innerHTML = "Obvod je " + o + " cm" + '<br/>' + "Obsah je " + s + " cm" ;
+    document.getElementById("lead2").innerHTML = "Obvod je " + o + " cm" + '<br>' + "Obsah je " + s + " cm<sup>2</sup>" ;
 });
 
 
